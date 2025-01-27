de2f4f5e1992e2641688c284353ce5a66be20e8c
TranQuan0406 <154067599+tranquan04062009@users.noreply.github.com>
2025-01-27 17:50:11 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -6,56 +6,16 @@ import subprocess
 import requests
 from bs4 import BeautifulSoup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-bot_token = '6393252222:AAEWYuwEUdVj7jN0AnhUzO5TPm9E0cOQPjo' 
+bot_token = '7766543633:AAFnN9tgGWFDyApzplak0tiJTafCxciFydo' 
 bot = telebot.TeleBot(bot_token)
 processes = []
-ADMIN_ID = '6452283369'
-allowed_group_id = -1001999335910
+
 def TimeStamp():
     now = str(datetime.date.today())
     return now
 
-@bot.message_handler(commands=['getkey'])
-def startkey(message):
-    bot.reply_to(message, text='VUI LÒNG ĐỢI TRONG GIÂY LÁT!')
-    key = "HDT-" + str(int(message.from_user.id) * int(datetime.date.today().day) - 12666)
-    key = "http://off-vn.x10.mx/index.html?key=" + key
-    print(key)
-    api_token = '18e951d7-7b48-4f87-8401-c584158244dd'    
-    url = requests.get(f'https://web1s.com/api?token={api_token}&url={key}').json()
-    url_key = url['shortenedUrl']
-    text = f'''
-- http://off-vn.x10.mx/n.mp4 LINK LẤY KEY {TimeStamp()} LÀ: {url_key}
-- KHI LẤY KEY XONG, DÙNG LỆNH /key <key> ĐỂ TIẾP TỤC
-'''
-    bot.reply_to(message, text)
-
-
-@bot.message_handler(commands=['key'])
-def key(message):
-    if len(message.text.split()) == 1:
-        bot.reply_to(message, 'VUI LÒNG NHẬP KEY.')
-        return
-
-    user_id = message.from_user.id
-
-    key = message.text.split()[1]
-    username = message.from_user.username
-    expected_key = "HDT-" + str(int(message.from_user.id) * int(datetime.date.today().day) - 12666)
-    if key == expected_key:
-        bot.reply_to(message, f' http://off-vn.x10.mx/t.mp4 [ KEY HỢP LỆ ] NGƯỜI DÙNG CÓ ID: [ {user_id} ] ĐƯỢC PHÉP ĐƯỢC SỬ DỤNG LỆNH    [ /spam ]')
-        fi = open(f'./user/{datetime.date.today().day}/{user_id}.txt',"w")
-        fi.write("")
-        fi.close()
-    else:
-        bot.reply_to(message, 'KEY KHÔNG HỢP LỆ.')
-  
 @bot.message_handler(commands=['spam'])
 def spam(message):
-    user_id = message.from_user.id
-    if not os.path.exists(f"./user/{datetime.date.today().day}/{user_id}.txt"):
-      bot.reply_to(message, 'Dùng /getkey Để Lấy Key Và Dùng /key Để Nhập Key Hôm Nay!')
-      return
     if len(message.text.split()) == 1:
         bot.reply_to(message, 'VUI LÒNG NHẬP SỐ ĐIỆN THOẠI ')
         return
@@ -82,14 +42,12 @@ def spam(message):
     file_path = os.path.join(os.getcwd(), "sms.py")
     process = subprocess.Popen(["python", file_path, phone_number, "100"])
     processes.append(process)
-    bot.reply_to(message, f'➤ USER ID: [ {user_id} ]\n➤ Tấn Công SĐT: [ {phone_number} ] Thành Công ✅\n➤ Lặp Lại : {lap} ⏰\n➤ Ngày : {TimeStamp()}\n http://off-vn.x10.mx/i.mp4\n')
+    bot.reply_to(message, f'➤ Tấn Công SĐT: [ {phone_number} ] Thành Công ✅\n➤ Lặp Lại : {lap} ⏰\n➤ Ngày : {TimeStamp()}\n http://off-vn.x10.mx/i.mp4\n')
 @bot.message_handler(commands=['help'])
 def help(message):
     help_text = '''
 Danh sách lệnh:
 ┏━━━━━━━━━━━━━━━━━┓
-┣➤/getkey: Để Lấy Key ✅
-┣➤/key: Để Nhập Key ✅
 ┣➤/spam {SĐT} {Số Lần} ✅
 ┣➤/help: Danh sách lệnh ✅
 ┣➤/time Thời Gian HĐ Bot ✅
@@ -102,10 +60,6 @@ Danh sách lệnh:
 # status
 @bot.message_handler(commands=['status'])
 def status(message):
-    user_id = message.from_user.id
-    if str(user_id) != ADMIN_ID:
-        bot.reply_to(message, 'Bạn không có quyền sử dụng lệnh này.')
-        return
     process_count = len(processes)
     bot.reply_to(message, f'Số quy trình đang chạy: {process_count}.')
 
@@ -124,10 +78,9 @@ def show_uptime(message):
 @bot.message_handler(commands=['admin'])
 def admin_info(message):
     # Thay thế các giá trị sau bằng thông tin liên hệ của bạn
-    zalo_box = "https://zalo.me/g/bprmyn080"
-    youtube_url = "https://youtube.com/@HDT-TOOL-VN?si=MvEhEk8SIMp2V26s"
-    web_url = "https://linkbio.co/sharetool"
-    admin_message = f"Thông tin liên hệ của Admin:\n\nBox Zalo: {zalo_box}\nweb: {web_url}\nyoutube: {youtube_url}"
+    youtube_url = "https://youtube.com/@secphiphai?si=Tum3SHPTFgsyv6Nl"
+    web_url = "https://linktr.ee/tranquan46"
+    admin_message = f"Thông tin liên hệ của Admin:\nweb: {web_url}\nyoutube: {youtube_url}"
     bot.reply_to(message, admin_message)
 # lo 
 @bot.message_handler(func=lambda message: True)
